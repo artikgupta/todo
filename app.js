3160329722e801130b5c6d0159c663ad844fad1d
artikgupta <artig9262@gmail.com>
2020-04-29 00:11:53 +0530

    edit todo and add complete button

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let submitBtn = document.getElementById("submit-btn");
+let filterCompleteBtn = document.getElementById("filter_completed");
 
 submitBtn.addEventListener("click", addTodo);
 
@@ -9,26 +10,40 @@ function addTodo() {
   let userInput = inputField.value;
   if (userInput === "") {
     alert("Your input field is empty , please enter a valid input");
+  } else {
+    let todoData = {
+      todoText: userInput,
+      isCompleted: false,
+    };
+    arr.push(todoData);
+    inputField.value = "";
+    displayTodo(arr);
   }
-  let todoData = {
-    todoText: userInput,
-    isCompleted: false,
-  };
-  arr.push(todoData);
-  inputField.value = "";
-  displayTodo();
+}
+function editTodo(i) {
+  let editValue = prompt("edit todo");
+  arr[i].todoText = editValue;
+  displayTodo(arr);
 }
 
 function deleteTodo(i) {
   arr.splice(i, 1);
-  console.log(arr);
-  displayTodo();
+  // console.log(arr);
+  displayTodo(arr);
 }
 
-function displayTodo() {
+function filterCompletedTodo() {
+  let completedTodos = arr.filter((todo) => todo.isCompleted == true);
+  console.log(completedTodos);
+  displayTodo(completedTodos);
+}
+filterCompleteBtn.addEventListener("click", filterCompletedTodo);
+
+function displayTodo(todoArray) {
   let todoList = document.getElementById("todolist");
   todoList.innerHTML = "";
-  for (let i = 0; i < arr.length; i++) {
+
+  for (let i = 0; i < todoArray.length; i++) {
     let listItem = document.createElement("li");
 
     let div = document.createElement("div");
@@ -37,21 +52,28 @@ function displayTodo() {
 
     let checkBox = document.createElement("input");
     checkBox.type = "checkbox";
+    checkBox.checked = todoArray[i].isCompleted;
     checkBox.addEventListener("change", () => {
-      arr[i].isCompleted = !arr[i].isCompleted;
-      console.log(arr);
+      todoArray[i].isCompleted = !todoArray[i].isCompleted;
+      // console.log(arr);
     });
 
     div.appendChild(checkBox);
 
     let paragraph = document.createElement("p");
-    paragraph.innerHTML = arr[i].todoText;
-
+    paragraph.innerHTML = todoArray[i].todoText;
+    // console.log(arr[i]);
+    // paragraph.innerHTML = arr[i].editValue;
+    // edit line 54
     div.appendChild(paragraph);
 
     let writeButton = document.createElement("button");
+    writeButton.classList.add("writeIcon");
     writeButton.innerHTML = '<i class="far fa-edit"></i>';
     div.appendChild(writeButton);
+    writeButton.addEventListener("click", () => {
+      editTodo(i);
+    });
 
     let deleteButton = document.createElement("button");
     deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
